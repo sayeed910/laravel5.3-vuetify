97431aba9cba6a715337bbf795906769f1e31740
Tahsin Al Sayeed <tahsinalsayeed@gmail.com>
2018-11-08 07:07:18 +0600

    configure vue project

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false
+    lintOnSave: false,
+
+    // output built static files to Laravel's public dir.
+    // note the "build" script in package.json needs to be modified as well.
+    outputDir: '../public',
+
+    // modify the location of the generated HTML file.
+    // make sure to do this only in production.
+    indexPath: process.env.NODE_ENV === 'production'
+        ? '../resources/views/index.blade.php'
+        : 'index.html'
 };
