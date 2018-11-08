6803b3bb2966c59fccf8b81578f8af0aa25be289
Tahsin Al Sayeed <tahsinalsayeed@gmail.com>
2018-11-08 07:52:29 +0600

    allow CORS request for development

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -42,6 +42,13 @@ $app->singleton(
 );
 
 /*
+ * Allow CORS for localhost only
+ */
+header('Access-Control-Allow-Origin: http://localhost:8080');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
+
+/*
 |--------------------------------------------------------------------------
 | Return The Application
 |--------------------------------------------------------------------------
